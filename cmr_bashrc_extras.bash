5439839ae616e62291c6c1216750fede6ba0370f
Ben Woodcroft <benjwoodcroft@gmail.com>
2026-07-29 10:39:26 +1000

    pixi_central: Revert what appears to be a breaking change to $PATH.

--- cmr_bashrc_extras.bash
+++ cmr_bashrc_extras.bash
@@ -3,9 +3,9 @@ export PATH="/mnt/hpccs01/work/microbiome/sw/hpc_scripts/bin:$PATH"
 # Add zellij
 export PATH="/work/microbiome/sw/zellij:$PATH"
 # Add general software from pixi
-export PATH="/pkg/cmr/sw/pixi_central/.pixi/envs/default/bin:$PATH"
+# export PATH="/pkg/cmr/sw/pixi_central/.pixi/envs/default/bin:$PATH"
 
-#function for command prompt and email notification of job completion
+# function for command prompt and email notification of job completion
 function notify { command "$@" && success || fail; }
 
 # Save all the history, see https://debian-administration.org/article/543/Bash_eternal_history
