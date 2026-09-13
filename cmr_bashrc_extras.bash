2495148f0a49bdc47f3fb6a91f308a31b617acfc
Ben Woodcroft <benjwoodcroft@gmail.com>
2026-09-14 09:34:44 +1000

    cmr_bashrc_extras.bash: add mksqhasfs alias

--- cmr_bashrc_extras.bash
+++ cmr_bashrc_extras.bash
@@ -5,6 +5,7 @@ export PATH="/work/microbiome/sw/zellij:$PATH"
 # Add general software from pixi
 # export PATH="/pkg/cmr/sw/pixi_central/.pixi/envs/default/bin:$PATH"
 alias aws='pixi run --manifest-path /pkg/cmr/sw/pixi_central/pixi.toml aws'
+alias mksquashfs='pixi run --manifest-path /pkg/cmr/sw/pixi_central/pixi.toml mksquashfs'
 
 # function for command prompt and email notification of job completion
 function notify { command "$@" && success || fail; }
